283a0348192ac9ff1ff0aefc3bcf76b5a474e849
Amar Pandav <amarpandav@gmail.com>
2017-07-24 20:59:14 +0200

    upgraded the dependencies

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MessageService} from "../shared/services/message.service";
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit() {
+    this.messageService.success("Welcome to my app", "Greetings")
   }
 
 }
