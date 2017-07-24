1cef82aa0adb69287c202b8853bf3c48411c3f11
Amar Pandav <amarpandav@gmail.com>
2017-07-24 16:22:43 +0200

    initial commit

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,19 +2,20 @@ package com.example;
 
 import com.example.config.Profiles;
 import com.example.testdata.TestDataService;
+import org.hsqldb.util.DatabaseManagerSwing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
-import org.hsqldb.util.DatabaseManagerSwing;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+
 /**
  * This class is both a bootstrap class and a configuration class.
- *
- *
  */
 
 //Enable component-scanning and auto-configuration
@@ -23,29 +24,32 @@ import org.springframework.core.env.Environment;
 //Unable to start EmbeddedWebApplicationContext due to missing EmbeddedServletContainerFactory bean.
 public class Application {
 
-    @Autowired
-    private Environment env;
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-    @Autowired
-    TestDataService testDataCreator;
+  @Autowired
+  private Environment env;
 
-	public static void main(String[] args) {
-	    //Bootstrap the application
-		//SpringApplication.run(Application.class, args);
+  @Autowired
+  TestDataService testDataCreator;
 
-        //When i used DatabaseManagerSwing I got java.awt.HeadlessException hence using SpringApplicationBuilder
-        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
-        builder.headless(false);
-        ConfigurableApplicationContext context = builder.run(args);
-	}
+  public static void main(String[] args) {
+    //Bootstrap the application
+    //SpringApplication.run(Application.class, args);
 
-    @PostConstruct
-    public void getDbManager(){
-        if (env.acceptsProfiles(Profiles.DEVELOPMENT)) {
-            testDataCreator.createTestData();
-            DatabaseManagerSwing.main(
-                    new String[] { "--url", "jdbc:hsqldb:mem:application", "--user", "sa", "--password", ""});
+    //When i used DatabaseManagerSwing I got java.awt.HeadlessException hence using SpringApplicationBuilder
+    SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+    builder.headless(false);
+    ConfigurableApplicationContext context = builder.run(args);
+  }
 
-        }
+  @PostConstruct
+  public void getDbManager() {
+    logger.info("Spring active profiles: " + Arrays.toString(env.getActiveProfiles()));
+    if (env.acceptsProfiles(Profiles.DEVELOPMENT)) {
+      testDataCreator.createTestData();
+      DatabaseManagerSwing.main(
+        new String[]{"--url", "jdbc:hsqldb:mem:application", "--user", "sa", "--password", ""});
+
     }
+  }
 }
